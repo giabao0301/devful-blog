432268a2596d7d4602ecbf23c75765ba2fe0f8c0
Gia Bao <21521866@gm.uit.edu.vn>
2024-04-29 14:52:34 +0700

    add new post

--- src/app/categories/[slug]/page.js
+++ src/app/categories/[slug]/page.js
@@ -26,41 +26,46 @@ export async function generateStaticParams() {
 
 export async function generateMetadata({ params }) {
   return {
-    title: `${params.slug.replaceAll("-"," ")} Blogs`,
-    description: `Learn more about ${params.slug === "all" ? "web development" : params.slug} through our collection of expert blogs and tutorials`,
+    title: `${params.slug.replaceAll("-", " ")} Blogs`,
+    description: `Learn more about ${
+      params.slug === "all" ? "web development" : params.slug
+    } through our collection of expert blogs and tutorials`,
   };
 }
 
-
 const CategoryPage = ({ params }) => {
-// Separating logic to create list of categories from all blogs
-const allCategories = ["all"]; // Initialize with 'all' category
-allBlogs.forEach(blog => {
-  blog.tags.forEach(tag => {
-    const slugified = slug(tag);
-    if (!allCategories.includes(slugified)) {
-      allCategories.push(slugified);
-    }
+  // Separating logic to create list of categories from all blogs
+  const allCategories = ["all"]; // Initialize with 'all' category
+  allBlogs.forEach((blog) => {
+    blog.tags.forEach((tag) => {
+      const slugified = slug(tag);
+      if (!allCategories.includes(slugified)) {
+        allCategories.push(slugified);
+      }
+    });
   });
-});
 
-// Sort allCategories to ensure they are in alphabetical order
-allCategories.sort();
+  // Sort allCategories to ensure they are in alphabetical order
+  allCategories.sort();
 
-// Step 2: Filter blogs based on the current category (params.slug)
-const blogs = allBlogs.filter(blog => {
-  if (params.slug === "all") {
-    return true; // Include all blogs if 'all' category is selected
-  }
-  return blog.tags.some(tag => slug(tag) === params.slug);
-});
+  // Step 2: Filter blogs based on the current category (params.slug)
+  const blogs = allBlogs.filter((blog) => {
+    if (params.slug === "all") {
+      return true; // Include all blogs if 'all' category is selected
+    }
+    return blog.tags.some((tag) => slug(tag) === params.slug);
+  });
 
   return (
     <article className="mt-12 flex flex-col text-dark dark:text-light">
       <div className=" px-5 sm:px-10  md:px-24  sxl:px-32 flex flex-col">
-        <h1 className="mt-6 font-semibold text-2xl md:text-4xl lg:text-5xl">#{params.slug}</h1>
+        <h1 className="mt-6 font-semibold text-2xl md:text-4xl lg:text-5xl">
+          #{params.slug}
+        </h1>
         <span className="mt-2 inline-block">
-          Discover more categories and expand your knowledge!
+          Khám phá những bài viết về{" "}
+          {params.slug === "all" ? "web development" : params.slug} qua bộ sưu
+          tập blog và hướng dẫn chuyên nghiệp của chúng tôi
         </span>
       </div>
       <Categories categories={allCategories} currentSlug={params.slug} />
