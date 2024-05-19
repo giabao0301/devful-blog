9d969c166d03718cff6cd23bef51a308ae2eaa8c
Gia Bao <21521866@gm.uit.edu.vn>
2024-05-19 09:08:15 +0700

    chore: Update privacy policy page layout and content

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -1,20 +1,42 @@
 const PrivacyPolicy = () => {
   return (
-    <div className="my-12 mx-10 h-screen">
+    <div className="my-12 mx-24 h-screen">
       <h2 className="text-5xl text-center font-bold">
         Chính sách quyền riêng tư
       </h2>
-      <div className="mt-12 flex flex-col gap-4">
-        <p className="text-2xl">
+      <div className="mt-12 flex flex-col gap-6">
+        <p className="text-xl">
           Chúng tôi rất coi trọng việc bảo vệ thông tin cá nhân của bạn. Chính
           sách quyền riêng tư này mô tả cách chúng tôi thu thập, sử dụng và bảo
           vệ thông tin cá nhân của bạn khi bạn sử dụng trang web của chúng tôi.
         </p>
-        <h3 className="text-3xl font-bold">Thu thập thông tin cá nhân</h3>
-        <p className="text-2xl">
+        <h3 className="text-2xl font-bold">Thu thập thông tin cá nhân</h3>
+        <p className="text-xl">
           Chúng tôi thu thập thông tin cá nhân từ bạn khi bạn đăng ký nhận bản
           tin trên trang web của chúng tôi. Thông tin này chỉ bao gồm địa chỉ
           email.
+        </p>
+        <h3 className="text-2xl font-bold">Sử dụng thông tin cá nhân</h3>
+        <p className="text-xl">
+          Chúng tôi sử dụng thông tin cá nhân của bạn để gửi cho bạn bản tin
+          thông tin hàng ngày.
+        </p>
+        <h3 className="text-2xl font-bold">Chia sẻ thông tin cá nhân</h3>
+        <p className="text-xl">
+          Chúng tôi không chia sẻ thông tin cá nhân của bạn với bất kỳ bên thứ
+          ba nào.
+        </p>
+        <h3 className="text-2xl font-bold">Bảo mật thông tin cá nhân</h3>
+        <p className="text-xl">
+          Chúng tôi bảo mật thông tin cá nhân của bạn bằng cách sử dụng các biện
+          pháp bảo mật hàng đầu. Chúng tôi không lưu trữ thông tin cá nhân của
+          bạn trên máy chủ của mình.
+        </p>
+        <h3 className="text-2xl font-bold">Liên hệ</h3>
+        <p className="text-xl">
+          Nếu bạn có bất kỳ câu hỏi hoặc đề xuất nào về chính sách quyền riêng
+          tư của chúng tôi, vui lòng liên hệ với chúng tôi qua email:
+          trinhpao246@gmail.com
         </p>
       </div>
     </div>
